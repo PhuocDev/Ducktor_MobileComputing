b3244c820eccbbb04b2924239cf388fb3f7927db
Bui Phuoc <buiphuoc@Phuocs-Mac.local>
2022-11-05 16:20:01 +0700

    demo

---
+++ src/test/java/com/example/mobilecomputing/MobileComputingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobilecomputing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileComputingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
