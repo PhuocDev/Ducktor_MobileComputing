953887249ef5d9a86558d4342420a995ac4446fd
Bui Phuoc <buiphuoc@Phuocs-Mac.local>
2022-11-05 16:30:14 +0700

    first commit

--- src/main/java/com/example/mobilecomputing/MobileComputingApplication.java
+++ src/main/java/com/example/mobilecomputing/MobileComputingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MobileComputingApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MobileComputingApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MobileComputingApplication.class, args);
+	}
 
 }
